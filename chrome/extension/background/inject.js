51ea1a970543ef5d438d099a5c0460b413aac845
Joe Karlsson <josephcarlson1@gmail.com>
2018-08-22 15:05:37 -0500

    added git checks.

--- chrome/extension/background/inject.js
+++ chrome/extension/background/inject.js
@@ -3,7 +3,7 @@ function isInjected(tabId) {
     code: `var injected = window.reactExampleInjected;
       window.reactExampleInjected = true;
       injected;`,
-    runAt: 'document_start'
+    runAt: 'document_start',
   });
 }
 
@@ -13,20 +13,26 @@ function loadScript(name, tabId, cb) {
   } else {
     // dev: async fetch bundle
     fetch(`http://localhost:3000/js/${name}.bundle.js`)
-    .then(res => res.text())
-    .then((fetchRes) => {
-      // Load redux-devtools-extension inject bundle,
-      // because inject script and page is in a different context
-      const request = new XMLHttpRequest();
-      request.open('GET', 'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd/js/redux-devtools-extension.js');  // sync
-      request.send();
-      request.onload = () => {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-          chrome.tabs.executeScript(tabId, { code: request.responseText, runAt: 'document_start' });
-        }
-      };
-      chrome.tabs.executeScript(tabId, { code: fetchRes, runAt: 'document_end' }, cb);
-    });
+      .then(res => res.text())
+      .then((fetchRes) => {
+        // Load redux-devtools-extension inject bundle,
+        // because inject script and page is in a different context
+        const request = new XMLHttpRequest();
+        request.open(
+          'GET',
+          'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd/js/redux-devtools-extension.js',
+        ); // sync
+        request.send();
+        request.onload = () => {
+          if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            chrome.tabs.executeScript(tabId, {
+              code: request.responseText,
+              runAt: 'document_start',
+            });
+          }
+        };
+        chrome.tabs.executeScript(tabId, { code: fetchRes, runAt: 'document_end' }, cb);
+      });
   }
 }
 
@@ -38,5 +44,6 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   const result = await isInjected(tabId);
   if (chrome.runtime.lastError || result[0]) return;
 
+  // eslint-disable-next-line no-console
   loadScript('inject', tabId, () => console.log('load inject bundle success!'));
 });
