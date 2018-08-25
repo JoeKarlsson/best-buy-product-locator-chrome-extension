0b00991739014bf1fcdc99c6576d63d45adb36b9
Joe Karlsson <josephcarlson1@gmail.com>
2018-08-25 10:40:12 -0500

    updated React.

--- app/components/CallToAction/CallToAction.js
+++ app/components/CallToAction/CallToAction.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { COLORS } from '../../constants/styles';
 
@@ -27,5 +28,7 @@ CallToAction.propTypes = {
   nearestStoreMapUrl: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
 };
+
+CallToAction.defaultProps = {};
 
 export default CallToAction;
