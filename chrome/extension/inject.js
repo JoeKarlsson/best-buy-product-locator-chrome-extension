9b895ad68206ddd95fe074bca760d7d5e858768c
Joe Karlsson <josephcarlson1@gmail.com>
2018-08-27 23:17:38 -0500

    removed commented out code.

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -1,7 +1,6 @@
 import getModelNumber from './get-model';
 
 window.addEventListener('load', () => {
-  console.log('hit');
   const script = document.createElement('script');
   script.innerHTML = getModelNumber;
   document.body.appendChild(script);
