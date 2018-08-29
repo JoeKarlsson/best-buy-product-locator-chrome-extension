4a50435d716bbd386b1a8b51a881615c7f240ac6
Joe Karlsson <josephcarlson1@gmail.com>
2018-08-28 20:35:26 -0500

    badge working.

--- chrome/extension/background/badge.js
+++ chrome/extension/background/badge.js
@@ -1,5 +1,4 @@
 chrome.storage.local.get('modelNumbers', (modelNumbers) => {
-  chrome.browserAction.setBadgeText({ text: '3' });
   if (modelNumbers.length > 0) {
     chrome.browserAction.setBadgeText({ text: ' ' });
   }
