2392d2529417def56b4d30823e6bded2f698f34b
Joe Karlsson <josephcarlson1@gmail.com>
2018-10-19 16:02:13 -0500

    feature complete.

--- app/components/CallToAction/CallToAction.js
+++ app/components/CallToAction/CallToAction.js
@@ -50,20 +50,31 @@ class CallToAction extends React.PureComponent {
 
   render() {
     const {
-      hours, isPopup, nearestStoreMapUrl, nearestStore, price
+      hours, isPopup, nearestStoreMapUrl, nearestStore, price, validStore
     } = this.props;
+
+    if (validStore) {
+      return (
+        <Container isPopup={isPopup}>
+          <H2>Want it faster? </H2>
+          <p>
+            This product is available for pick up at the{' '}
+            <a target="_blank" rel="noopener noreferrer" href={nearestStoreMapUrl}>
+              Best Buy in {nearestStore}.
+            </a>{' '}
+            {this.formatMessage(hours, validStore)}
+            .&nbsp;
+          </p>
+          <p>
+            Price: <b>${price}</b>
+          </p>
+        </Container>
+      );
+    }
 
     return (
       <Container isPopup={isPopup}>
-        <H2>Want it faster? </H2>
-        <p>
-          This product is available for pick up at the{' '}
-          <a target="_blank" rel="noopener noreferrer" href={nearestStoreMapUrl}>
-            Best Buy in {nearestStore}.
-          </a>{' '}
-          {this.formatMessage(hours)}
-          .&nbsp;
-        </p>
+        <H2>Order now on BestBuy.com</H2>
         <p>
           Price: <b>${price}</b>
         </p>
@@ -78,6 +89,7 @@ CallToAction.propTypes = {
   nearestStore: PropTypes.string.isRequired,
   nearestStoreMapUrl: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  validStore: PropTypes.bool.isRequired,
 };
 
 CallToAction.defaultProps = {
