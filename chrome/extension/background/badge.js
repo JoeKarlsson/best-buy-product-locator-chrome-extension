3dc40d08010df100d00788d5af4bd04af94a8006
Eric Moore <moore.ericc@gmail.com>
2018-12-16 21:46:53 -0600

    WIP

--- chrome/extension/background/badge.js
+++ chrome/extension/background/badge.js
@@ -1,5 +1,14 @@
-chrome.storage.local.get('modelNumbers', (modelNumbers) => {
-  if (modelNumbers.length > 0) {
-    chrome.browserAction.setBadgeText({ text: ' ' });
+import { COLORS } from '../../../app/constants/styles';
+
+chrome.storage.local.get('productCode', (productCode) => {
+  if (productCode) {
+    chrome.browserAction.setBadgeText({ text: '1' });
+    chrome.browserAction.setBadgeBackgroundColor({
+      color: COLORS.red,
+    });
   }
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.browserAction.setBadgeText({ text: '' });
 });
