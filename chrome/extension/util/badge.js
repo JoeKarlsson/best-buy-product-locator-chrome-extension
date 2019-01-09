46002d5baef3ea390556996dae90dd307cf78052
Eric Moore <moore.ericc@gmail.com>
2019-01-08 23:31:58 -0600

    still some cleanup to do, but badges are behaving much more reliably

---
+++ chrome/extension/util/badge.js
@@ -0,0 +1,25 @@
+import { STORAGE_PREFIX } from '../constants/constants';
+import { COLORS } from '../../../app/constants/styles';
+
+export function updateBadgeSeen(tabId) {
+  chrome.storage.local.get([`${STORAGE_PREFIX}${tabId}`], (tabData) => {
+    if (Object.keys(tabData).length > 0 && !tabData[`${STORAGE_PREFIX}${tabId}`].badgeSeen) {
+      const data = tabData[`${STORAGE_PREFIX}${tabId}`];
+      chrome.storage.local.set({
+        [`${STORAGE_PREFIX}${tabId}`]: {
+          ...data,
+          badgeSeen: true,
+        },
+      });
+    }
+  });
+}
+
+export function setBadge(text = '1', color = COLORS.red) {
+  chrome.browserAction.setBadgeText({ text });
+  chrome.browserAction.setBadgeBackgroundColor({ color });
+}
+
+export function removeBadge() {
+  chrome.browserAction.setBadgeText({ text: '' });
+}
